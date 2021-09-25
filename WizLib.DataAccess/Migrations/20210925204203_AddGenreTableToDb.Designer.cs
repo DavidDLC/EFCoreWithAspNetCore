ef69419e3a8fe9184bcd15c47275e6b41d7d2fe6
David Cruz <david.cruz@unosquare.com>
2021-09-25 15:42:34 -0500

    Add genre model and add migration

--- WizLib.DataAccess/Migrations/20210925204203_AddGenreTableToDb.Designer.cs
+++ WizLib.DataAccess/Migrations/20210925204203_AddGenreTableToDb.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WizLib.DataAccess.Data;
 
 namespace WizLib.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210925204203_AddGenreTableToDb")]
+    partial class AddGenreTableToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
