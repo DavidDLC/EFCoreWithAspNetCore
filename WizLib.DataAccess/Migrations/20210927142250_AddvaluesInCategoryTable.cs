fc1ed26807958ee9376810f44b6fb45a402a024e
David Cruz <david.cruz@unosquare.com>
2021-09-27 09:32:10 -0500

    insert record with migration and logging check migration

---
+++ WizLib.DataAccess/Migrations/20210927142250_AddvaluesInCategoryTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizLib.DataAccess.Migrations
+{
+    public partial class AddvaluesInCategoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('cat 1')");
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('cat 2')");
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('cat 3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
